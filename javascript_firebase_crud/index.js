d55ce15baaa681cca4f3f65470756289934e7c9d
BrandonRoblesJurado <85750160+BrandonRoblesJurado@users.noreply.github.com>
2022-02-18 20:35:04 -0500

    Add files via upload

--- javascript_firebase_crud/index.js
+++ javascript_firebase_crud/index.js
@@ -1,10 +1,10 @@
 import {
+  savetask,
+  getTasks,
   onGetTasks,
-  saveTask,
   deleteTask,
   getTask,
   updateTask,
-  getTasks,
 } from "./firebase.js";
 
 const taskForm = document.getElementById("task-form");
@@ -13,87 +13,63 @@ const tasksContainer = document.getElementById("tasks-container");
 let editStatus = false;
 let id = "";
 
-window.addEventListener("DOMContentLoaded", async (e) => {
-  // const querySnapshot = await getTasks();
-  // querySnapshot.forEach((doc) => {
-  //   console.log(doc.data());
-  // });
-
+window.addEventListener("DOMContentLoaded", async () => {
   onGetTasks((querySnapshot) => {
-    tasksContainer.innerHTML = "";
-
+    tasksContainer.innerHTML = " ";
     querySnapshot.forEach((doc) => {
       const task = doc.data();
-
       tasksContainer.innerHTML += `
-      <div class="card card-body mt-2 border-primary">
-    <h3 class="h5">${task.title}</h3>
-    <p>${task.description}</p>
-    <div>
-      <button class="btn btn-primary btn-delete" data-id="${doc.id}">
-        🗑 Delete
-      </button>
-      <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
-        🖉 Edit
-      </button>
-    </div>
-  </div>`;
+      <div class="card card-body mt-2 boder-primary"> 
+        <h3 class = "h5">${task.title} </h3>
+        <p> ${task.description}</p>
+        <div>
+          <button class = "btn btn-primary btn-delete" data-id= "${doc.id}">Delete</button>
+          <button class = "btn btn-secondary btn-edit" data-id= "${doc.id}">Edit</button>
+        </div>
+      </div>
+    `;
     });
 
     const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
-    btnsDelete.forEach((btn) =>
-      btn.addEventListener("click", async ({ target: { dataset } }) => {
-        try {
-          await deleteTask(dataset.id);
-        } catch (error) {
-          console.log(error);
-        }
-      })
-    );
+    btnsDelete.forEach((btn) => {
+      btn.addEventListener("click", ({ target: { dataset } }) => {
+        deleteTask(dataset.id);
+      });
+    });
 
     const btnsEdit = tasksContainer.querySelectorAll(".btn-edit");
     btnsEdit.forEach((btn) => {
       btn.addEventListener("click", async (e) => {
-        try {
-          const doc = await getTask(e.target.dataset.id);
-          const task = doc.data();
-          taskForm["task-title"].value = task.title;
-          taskForm["task-description"].value = task.description;
+        const doc = await getTask(e.target.dataset.id);
+        const task = doc.data();
 
-          editStatus = true;
-          id = doc.id;
-          taskForm["btn-task-form"].innerText = "Update";
-        } catch (error) {
-          console.log(error);
-        }
+        taskForm["task-title"].value = task.title;
+        taskForm["task-description"].value = task.description;
+
+        editStatus = true;
+        id = doc.id;
+
+        taskForm["btn-task-save"].innerText = "Update";
       });
     });
   });
 });
 
-taskForm.addEventListener("submit", async (e) => {
+taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const title = taskForm["task-title"];
   const description = taskForm["task-description"];
 
-  try {
-    if (!editStatus) {
-      await saveTask(title.value, description.value);
-    } else {
-      await updateTask(id, {
-        title: title.value,
-        description: description.value,
-      });
-
-      editStatus = false;
-      id = "";
-      taskForm["btn-task-form"].innerText = "Save";
-    }
-
-    taskForm.reset();
-    title.focus();
-  } catch (error) {
-    console.log(error);
+  if (!editStatus) {
+    savetask(title.value, description.value);
+  } else {
+    updateTask(id, {
+      title: title.value,
+      description: description.value,
+    });
+    editStatus = false;
   }
+
+  taskForm.reset();
 });
